a4395f1392dbefe61011a792ec5d172dc2f17023
root <max.marwitz@gmail.com>
2021-08-29 15:10:13 -0400

    added props.js

---
+++ props.js
@@ -0,0 +1,10 @@
+function Prop(id, name, description) {
+  this.id = id;
+  this.name = name;
+  this.description = description;
+}
+
+
+
+//Room 1
+roomlist[0].props.push(new Prop(0,'trashcan', "It's a heavily dented trashcan on it's side."));
