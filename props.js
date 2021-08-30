1a3fcb1af506074a97e2208ba981b27d979366eb
root <max.marwitz@gmail.com>
2021-08-30 11:56:25 -0400

    Added the use function to props

--- props.js
+++ props.js
@@ -8,8 +8,18 @@ function Prop(id, name, description, roomdescription, shortdesc, item, usable) {
   this.usable = usable;
 }
 
+Prop.prototype.use = function(Prop){
+  // this | refers to the object that is calling the function.
+  // Prop | refers to the other object that is referenced.
+  // Need to generalize the add and remove functions from rooms and inventory so that it can be used here.
+  if(this.usable == true && Prop.usable == true){
+    console.log(Prop.name + ' is smashed against ' + this.name);
+  }else{
+    console.log("No can do scooby doo");
+  }
+}
 
 
 //Room 1
-roomlist[0].props.push(new Prop(0,'trashcan', "It's a heavily dented trashcan with the word Excalican carved into it.", "A heavily dented trashcan is laying on it's side.", "It's a trashcan genius.", true, false));
+roomlist[0].props.push(new Prop(0,'trashcan', "It's a heavily dented trashcan with the word Excalican carved into it.", "A heavily dented trashcan is laying on it's side.", "It's a trashcan genius.", true, true));
 roomlist[0].props.push(new Prop(1,'rock', "It's your best friend, it has eyes and everything. You have named it Hugh.", "A small rock looks at you with hope in it's eyes.", "It's a rock.", true, true));
